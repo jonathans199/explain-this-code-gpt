915c5c4d4217e8ca992fc85a36b20f74fd043fbd
jonathans199 <jso1919@gmail.com>
2023-04-13 15:08:28 -0400

    first commit

---
+++ src/pages/api/explain.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Configuration, OpenAIApi, ChatCompletionResponseMessage } from 'openai'
+
+const configuration = new Configuration({ apiKey: process.env.GPT_API_KEY })
+const openai = new OpenAIApi(configuration)
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<ChatCompletionResponseMessage | undefined>
+) {
+	openai
+		.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: `explain this code: ${req.body}` }],
+		})
+		.then(response => res.status(200).send(response.data.choices[0].message))
+		.catch(err => console.error(err))
+}
